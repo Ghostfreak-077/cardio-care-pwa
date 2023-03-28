a2f6d25440a1dc763b122cd766fc9b97a39da6e8
Aryadeep Gogoi <100404453+Alsi-Aryadeep31@users.noreply.github.com>
2023-03-28 23:14:05 +0530

    updated auth

--- src/components/Signup/Signup.jsx
+++ src/components/Signup/Signup.jsx
@@ -3,12 +3,12 @@ import { Form } from 'react-router-dom'
 import styles from './Signup.module.scss'
 import addNotification from 'react-push-notification'
 import YouTube from 'react-youtube'
-import { createUserWithEmailAndPassword, signInWithCustomToken, signInWithEmailAndPassword } from 'firebase/auth'
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth'
 import { auth } from '../../firebase';
 
 const Signup = () => {
-  const [email, setEmail] = useState()
-  const [password, setPassword] = useState()
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
 
   // firebase.auth().useEmulator('http://localhost:9099', { disableWarnings: true });
   // firebase.auth().onAuthStateChanged((user) => {
@@ -19,34 +19,33 @@ const Signup = () => {
   // firebase.auth().settings.debug = true;
 
 
-  const handleSubmit = () => {
-    // signInWithEmailAndPassword(auth, email, password)
-    //   .then((userCredential) => {
-    //     // Signed in 
-    //     const user = userCredential.user;
-    //     console.log(user.getIdToken());
-    //     // ...
-    //   })
-    //   .catch((error) => {
-    //     const errorCode = error.code;
-    //     const errorMessage = error.message;
-    //     console.log(errorCode, errorMessage);
-    //     // ..
-    //   });
-    signInWithCustomToken(auth, "eyJhbGciOiJSUzI1NiIsImtpZCI6Ijk3OWVkMTU1OTdhYjM1Zjc4MjljZTc0NDMwN2I3OTNiN2ViZWIyZjAiLCJ0eXAiOiJKV1QifQ.eyJpc3MiOiJodHRwczovL3NlY3VyZXRva2VuLmdvb2dsZS5jb20vaGFsbG93ZWQtc2NlbmUtMzMwMDEzIiwiYXVkIjoiaGFsbG93ZWQtc2NlbmUtMzMwMDEzIiwiYXV0aF90aW1lIjoxNjc5OTAyMDc5LCJ1c2VyX2lkIjoiVUtWRHJoRUdZbWFzT3ppM3Z2YWZqaVBSSzZ4MSIsInN1YiI6IlVLVkRyaEVHWW1hc096aTN2dmFmamlQUks2eDEiLCJpYXQiOjE2Nzk5MDIwNzksImV4cCI6MTY3OTkwNTY3OSwiZW1haWwiOiJhZG1pbjJAZ21haWwuY29tIiwiZW1haWxfdmVyaWZpZWQiOmZhbHNlLCJmaXJlYmFzZSI6eyJpZGVudGl0aWVzIjp7ImVtYWlsIjpbImFkbWluMkBnbWFpbC5jb20iXX0sInNpZ25faW5fcHJvdmlkZXIiOiJwYXNzd29yZCJ9fQ.Qu-tYByY1SD8lesW5jjHJoyF3PrqLizAuTJvbmRv6Gga6SZ0KhkssjGymxbXsAEXEZY5H1e_4fdpMwxSY-DSkkZKWgLjzwja8Lbxb5ei83PCGEiqi4tw2Ycp-89QZsJTEvs4LvFmpOPNv-JmBb8fz870nLmphai_QpuIBCCV-NxejdYnnBaMA3-Y3tLTE9fEOSHJ1WMqAa8aG99STFc86KbKZutBZsdv4j2uXLmt7sUKGTOMnCgoH8Mu0hZVAy4z33PSjC89Lc-0AQMyDlYZMWTQ135ZUD5l3LpJWEM2frIefje91ldWRpNtaZoE-f-p4OAddAurWNKqj3S5IU_uiw")
+
+
+  const signUp = (e) => {
+    e.preventDefault();
+    createUserWithEmailAndPassword(auth, email, password)
       .then((userCredential) => {
-        // Signed in
-        const user = userCredential.user;
-        console.log(user);
-        // ...
+        console.log(userCredential);
       })
       .catch((error) => {
-        const errorCode = error.code;
-        const errorMessage = error.message;
-        console.log(errorCode, errorMessage);
-        // ...
+        console.log(error);
       });
-  }
+  };
+
+  //   signInWithCustomToken(auth, "eyJhbGciOiJSUzI1NiIsImtpZCI6Ijk3OWVkMTU1OTdhYjM1Zjc4MjljZTc0NDMwN2I3OTNiN2ViZWIyZjAiLCJ0eXAiOiJKV1QifQ.eyJpc3MiOiJodHRwczovL3NlY3VyZXRva2VuLmdvb2dsZS5jb20vaGFsbG93ZWQtc2NlbmUtMzMwMDEzIiwiYXVkIjoiaGFsbG93ZWQtc2NlbmUtMzMwMDEzIiwiYXV0aF90aW1lIjoxNjc5OTAyMDc5LCJ1c2VyX2lkIjoiVUtWRHJoRUdZbWFzT3ppM3Z2YWZqaVBSSzZ4MSIsInN1YiI6IlVLVkRyaEVHWW1hc096aTN2dmFmamlQUks2eDEiLCJpYXQiOjE2Nzk5MDIwNzksImV4cCI6MTY3OTkwNTY3OSwiZW1haWwiOiJhZG1pbjJAZ21haWwuY29tIiwiZW1haWxfdmVyaWZpZWQiOmZhbHNlLCJmaXJlYmFzZSI6eyJpZGVudGl0aWVzIjp7ImVtYWlsIjpbImFkbWluMkBnbWFpbC5jb20iXX0sInNpZ25faW5fcHJvdmlkZXIiOiJwYXNzd29yZCJ9fQ.Qu-tYByY1SD8lesW5jjHJoyF3PrqLizAuTJvbmRv6Gga6SZ0KhkssjGymxbXsAEXEZY5H1e_4fdpMwxSY-DSkkZKWgLjzwja8Lbxb5ei83PCGEiqi4tw2Ycp-89QZsJTEvs4LvFmpOPNv-JmBb8fz870nLmphai_QpuIBCCV-NxejdYnnBaMA3-Y3tLTE9fEOSHJ1WMqAa8aG99STFc86KbKZutBZsdv4j2uXLmt7sUKGTOMnCgoH8Mu0hZVAy4z33PSjC89Lc-0AQMyDlYZMWTQ135ZUD5l3LpJWEM2frIefje91ldWRpNtaZoE-f-p4OAddAurWNKqj3S5IU_uiw")
+  //     .then((userCredential) => {
+  //       // Signed in
+  //       const user = userCredential.user;
+  //       console.log(user);
+  //       // ...
+  //     })
+  //     .catch((error) => {
+  //       const errorCode = error.code;
+  //       const errorMessage = error.message;
+  //       console.log(errorCode, errorMessage);
+  //       // ...
+  //     });
+  // }
 
   // // below is a function handleSubmit to login using auth token in firebase
   // const handleSubmit = () => {
@@ -67,7 +66,7 @@ const Signup = () => {
     <div className={styles.signup}>
       <input type="email" value={email} onChange={(e) => { setEmail(e.target.value) }} />
       <input type="password" value={password} onChange={(e) => { setPassword(e.target.value) }} />
-      <button onClick={handleSubmit}>Submit</button>
+      <button onClick={(e)=>signUp(e)}>Submit</button>
       <h1>{email}</h1>
       <h1>{password}</h1>
     </div>
--- src/firebase.js
+++ src/firebase.js
@@ -1,41 +1,69 @@
-// Import the functions you need from the SDKs you need
+// // Import the functions you need from the SDKs you need
+
+// import { initializeApp } from "firebase/app";
+// import {getAuth} from "firebase/auth";
+// import { getAnalytics } from "firebase/analytics";
+
+// // TODO: Add SDKs for Firebase products that you want to use
+
+// // https://firebase.google.com/docs/web/setup#available-libraries
+
+
+// // Your web app's Firebase configuration
+
+// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+// const firebaseConfig = {
+
+  // apiKey: "AIzaSyCMCZPhxP8yeW5o3JAmIJ_EGW3U250rIGg",
+
+  // authDomain: "hallowed-scene-330013.firebaseapp.com",
+
+  // projectId: "hallowed-scene-330013",
+
+  // storageBucket: "hallowed-scene-330013.appspot.com",
+
+  // messagingSenderId: "242399349460",
+
+  // appId: "1:242399349460:web:8b901ea26aa1d235d24941",
+
+  // measurementId: "G-JFD4NMTXN6"
+
+// };
+
+
+// // Initialize Firebase
+
+// export const app = initializeApp(firebaseConfig);
+
+// 
+
+// export const auth = getAuth(app); 
+
 
 import { initializeApp } from "firebase/app";
-import {getAuth} from "firebase/auth";
-import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth";
 
+// Your web app's Firebase configuration, replace it with your project keys
+// Import the functions you need from the SDKs you need
 // TODO: Add SDKs for Firebase products that you want to use
-
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-
 // Your web app's Firebase configuration
-
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-
 const firebaseConfig = {
-
-  apiKey: "AIzaSyCMCZPhxP8yeW5o3JAmIJ_EGW3U250rIGg",
-
-  authDomain: "hallowed-scene-330013.firebaseapp.com",
-
-  projectId: "hallowed-scene-330013",
-
-  storageBucket: "hallowed-scene-330013.appspot.com",
-
-  messagingSenderId: "242399349460",
-
-  appId: "1:242399349460:web:8b901ea26aa1d235d24941",
-
-  measurementId: "G-JFD4NMTXN6"
-
+  apiKey: "AIzaSyCRP0I_4UnDC1SdXDpRhZI29x9Gr3121m0",
+  authDomain: "react-auth-cardiocare.firebaseapp.com",
+  projectId: "react-auth-cardiocare",
+  storageBucket: "react-auth-cardiocare.appspot.com",
+  messagingSenderId: "51072271801",
+  appId: "1:51072271801:web:dbecbb8e4c000143c2a268"
 };
-
 
 // Initialize Firebase
 
-export const app = initializeApp(firebaseConfig);
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 
-export const analytics = getAnalytics(app);
-
-export const auth = getAuth(app); 
+// Initialize Firebase Authentication and get a reference to the service
+export const auth = getAuth(app);
+// export const analytics = getAnalytics(app);
