939aff34efaaf91a481f7d45217baa831d5d1080
Pragya Annesha Baruah <pragya.annesha@gmail.com>
2023-03-29 20:27:43 +0530

    ADD: News Api integrated

--- src/pages/Learning/LearnNews.jsx
+++ src/pages/Learning/LearnNews.jsx
@@ -1,53 +1,40 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import LearnButtons from '../../components/LearnButtons/LearnButtons';
 import styles from "./LearnNews.module.scss";
+import axios from "axios";
 
 const LearnNews = () => {
+  const [newsData, setNewsData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(
+          "https://newsapi.org/v2/everything?q=cardiac%20diseases&sortBy=publishedAt&apiKey=7d78b61fa1dc40418bc50c57e467511a"
+        );
+        setNewsData(response.data.articles);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchData();
+  }, []);
+
   return (
     <div className={styles.learnnews}>
        <LearnButtons/>
-      <div className={styles.newsContainer}>
-        <div className={styles.news}>
-          People exposed to low doses of ionizing radiation have an extra, but
-          modest, risk of developing heart disease during their lifetime,
-          according to a new study published by an international consortium of
-          researchers.
-        </div>
-      </div>
-      <div className={styles.newsContainer}>
-        <div className={styles.news}>
-          People exposed to low doses of ionizing radiation have an extra, but
-          modest, risk of developing heart disease during their lifetime,
-          according to a new study published by an international consortium of
-          researchers.
-        </div>
-      </div>
-      <div className={styles.newsContainer}>
-        <div className={styles.news}>
-          People exposed to low doses of ionizing radiation have an extra, but
-          modest, risk of developing heart disease during their lifetime,
-          according to a new study published by an international consortium of
-          researchers.
-        </div>
-      </div>
-      <div className={styles.newsContainer}>
-        <div className={styles.news}>
-          People exposed to low doses of ionizing radiation have an extra, but
-          modest, risk of developing heart disease during their lifetime,
-          according to a new study published by an international consortium of
-          researchers.
-        </div>
-      </div>
-      <div className={styles.newsContainer}>
-        <div className={styles.news}>
-          People exposed to low doses of ionizing radiation have an extra, but
-          modest, risk of developing heart disease during their lifetime,
-          according to a new study published by an international consortium of
-          researchers.
-        </div>
-      </div>
+       {newsData.length > 0 ? (
+          newsData.map((article, index) => (
+            <div key={index} className={styles.newsContainer}>
+              <div className={styles.news}>{article.title}</div>
+            </div>
+          ))
+        ) : (
+          <div>Loading news...</div>
+        )}
       <div className={styles.extension}></div>
     </div>
   );
 };
+
 export default LearnNews;
